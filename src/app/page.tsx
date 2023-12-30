00d209bee133532e58df0cfc9a295eb372dc2d06
danielbatres <danielbatresuser@gmail.com>
2023-12-30 14:37:09 -0600

    Props type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { RandomFox } from "@/components/RandomFox";
 
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
   return (
     <>
       <h1>Hello</h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
     </>
-  )
+  );
 }
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,8 +1,8 @@
-const random = (): number => Math.floor(Math.random() * 123) + 1;
+type Props = {
+  image: string
+};
 
-const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
-  
+const RandomFox = ({ image }: Props): JSX.Element => {
   return (
     <img width={320} height="auto" src={image} className="rounded" />
   );
