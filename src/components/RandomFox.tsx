0e1caacc5d15cdb2d6d0a8b8260160a230bd9f41
danielbatres <danielbatresuser@gmail.com>
2024-01-03 16:49:08 -0600

    Types for references and observers

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,10 +1,14 @@
+import { useRef } from "react";
+
 type Props = {
   image: string
 };
 
 const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+
   return (
-    <img width={320} height="auto" src={image} className="rounded" />
+    <img ref={node} width={320} height="auto" src={image} className="rounded" />
   );
 }
 
